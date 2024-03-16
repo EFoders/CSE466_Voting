08482583e844c2cf2813282ceb91b5ac8628a19c
EFoders <156714984+EFoders@users.noreply.github.com>
2024-03-16 18:09:34 -0400

    Updated all pages

--- PHP/landing.php
+++ PHP/landing.php
@@ -1,8 +1,6 @@
 <!--This may or may not be used depending on how advanced our landing page is -->
 <?php
-echo "<h2>PHP is Fun!</h2>";
-echo "Hello world!<br>";
-echo "I'm about to learn PHP!<br>";
-echo "This ", "string ", "was ", "made ", "with multiple parameters.";
-
-?>
+//echo "<h2>PHP is Fun!</h2>";
+//echo "Hello world!<br>";
+//echo "I'm about to learn PHP!<br>";
+//echo "This ", "string ", "was ", "made ", "with multiple parameters.";
